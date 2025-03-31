fd4851a04f4658d87f2e464efbfb95b908e8fdd2
kim-uijin <jindevst@gmail.com>
2025-03-31 18:08:23 +0900

    feat: 글로벌 타임존 설정

--- src/main/kotlin/com/jindevst/springbootcleanarchboilterplate/SpringbootCleanArchBoilterplateApplication.kt
+++ src/main/kotlin/com/jindevst/springbootcleanarchboilterplate/SpringbootCleanArchBoilterplateApplication.kt
@@ -1,10 +1,19 @@
 package com.jindevst.springbootcleanarchboilterplate
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.*
 
+@EnableJpaAuditing
 @SpringBootApplication
 class SpringbootCleanArchBoilterplateApplication
+
+@PostConstruct
+fun init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringbootCleanArchBoilterplateApplication>(*args)
